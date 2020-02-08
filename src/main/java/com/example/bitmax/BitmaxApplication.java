77c9884a8138f1aa2362e8da7705b9b695abfe09
xjeansantana <jean.santtana@gmail.com>
2020-02-08 16:44:38 -0300

    Initial commit

---
+++ src/main/java/com/example/bitmax/BitmaxApplication.java
@@ -0,0 +1,13 @@
+package com.example.bitmax;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BitmaxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BitmaxApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/bitmax/BitmaxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bitmax;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BitmaxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
