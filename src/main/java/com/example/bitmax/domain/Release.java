bb3da04e4fa9b81c84b006f6e55443a0621094e0
xjeansantana <jean.santtana@gmail.com>
2020-02-09 11:13:57 -0300

    O projeto Lombok

--- src/main/java/com/example/bitmax/domain/Release.java
+++ src/main/java/com/example/bitmax/domain/Release.java
@@ -1,8 +1,11 @@
 package com.example.bitmax.domain;
 
+import lombok.*;
+
 import javax.persistence.*;
 
 @Entity(name = "releases")
+@Data
 public class Release {
 
     @Id
@@ -12,36 +15,7 @@ public class Release {
     private String nome;
     private String tipo;
 
-    private  Release(){
 
-    }
 
-    public Release(Long id, String nome) {
-        this.id = id;
-        this.nome = nome;
-    }
 
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public String getTipo() {
-        return tipo;
-    }
-
-    public void setTipo(String tipo) {
-        this.tipo = tipo;
-    }
 }
