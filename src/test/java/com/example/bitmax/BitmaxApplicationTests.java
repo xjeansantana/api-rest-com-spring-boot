5bc4659608f8046cffee1ba8a80966ad1e314fb7
xjeansantana <jean.santtana@gmail.com>
2020-02-09 15:42:07 -0300

    Teste para Inserir uma release

--- src/test/java/com/example/bitmax/BitmaxApplicationTests.java
+++ src/test/java/com/example/bitmax/BitmaxApplicationTests.java
@@ -1,13 +1,30 @@
 package com.example.bitmax;
 
+import com.example.bitmax.domain.Release;
+import com.example.bitmax.domain.ReleaseService;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class BitmaxApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Autowired
+    private ReleaseService service;
+
+    @Test
+    public void teste1() {
+        Release release = new Release();
+        release.setNome("5.00");
+        release.setTipo("Empresarial");
+
+        service.insert(release);
+
+    }
+
+    @Test
+    public void teste2() {
+
+    }
 
 }
