a9b82ea95578582def1db44137cd0f3334a2db34
xjeansantana <jean.santtana@gmail.com>
2020-02-09 16:12:15 -0300

    Teste para inserir e Deletar uma release

--- src/test/java/com/example/bitmax/BitmaxApplicationTests.java
+++ src/test/java/com/example/bitmax/BitmaxApplicationTests.java
@@ -1,11 +1,25 @@
 package com.example.bitmax;
 
+
 import com.example.bitmax.domain.Release;
 import com.example.bitmax.domain.ReleaseService;
+import com.example.bitmax.domain.dto.ReleaseDTO;
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
+import static junit.framework.TestCase.*;
+
+
+
+import java.util.Optional;
+
+import static org.hibernate.validator.internal.util.Contracts.assertNotNull;
+
+
+@RunWith(SpringRunner.class)
 @SpringBootTest
 class BitmaxApplicationTests {
 
@@ -18,8 +32,34 @@ class BitmaxApplicationTests {
         release.setNome("5.00");
         release.setTipo("Empresarial");
 
-        service.insert(release);
+        ReleaseDTO r = service.insert(release);
 
+        assertNotNull(r);
+
+        Long id = r.getId();
+        assertNotNull(id);
+
+        //Buscar objeto
+        Optional<ReleaseDTO> op = service.getReleaseById(id);
+        assertTrue(op.isPresent());
+
+        r = op.get();
+        assertEquals ("5.00", r.getNome());
+        assertEquals("Empresarial", r.getTipo());
+
+        //deletar o objeto
+
+        service.delete(id);
+
+        // Verificar se deletou
+        assertFalse((service.getReleaseById(id).isPresent()));
+
+
+
+
+    }
+
+    private void assertTrue(boolean present) {
     }
 
     @Test
